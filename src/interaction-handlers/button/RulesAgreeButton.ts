26336d6342e43bc4023fbc5295b9397b64fed5d4
b1nzeex <cbeachx@gmail.com>
2024-04-16 11:12:07 +0100

    new account flags

--- src/interaction-handlers/button/RulesAgreeButton.ts
+++ src/interaction-handlers/button/RulesAgreeButton.ts
@@ -4,6 +4,7 @@ import {
 } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
 import { ButtonInteraction } from "discord.js";
+import { Time } from "@sapphire/time-utilities";
 
 @ApplyOptions<InteractionHandler.Options>({
   interactionHandlerType: InteractionHandlerTypes.Button,
@@ -17,6 +18,27 @@ export class RulesAgreeButton extends InteractionHandler {
     const member = interaction.guild.members.cache.get(interaction.user.id);
 
     if (!member.roles.cache.has(process.env.DISCORD_MEMBER_ROLE_ID)) {
+      const flagged = this.container.settings.get("global", "flagged", []);
+      if (flagged.includes(interaction.user.id)) {
+        return await interaction.reply({
+          content:
+            "You are flagged and cannot access the server! Please contact a member of staff for more information.",
+          ephemeral: true,
+        });
+      }
+
+      // Check if the users account is older than 14 days
+      if (Date.now() - interaction.user.createdTimestamp < Time.Day * 14) {
+        flagged.push(interaction.user.id);
+        this.container.settings.set("global", "flagged", flagged);
+
+        return await interaction.reply({
+          content:
+            "You are flagged and cannot access the server! Please contact a member of staff for more information.",
+          ephemeral: true,
+        });
+      }
+
       await member.roles.add(process.env.DISCORD_MEMBER_ROLE_ID);
 
       return await interaction.reply({
